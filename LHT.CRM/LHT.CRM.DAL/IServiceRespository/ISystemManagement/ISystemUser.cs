3cc6b56c14df6eb2b456046335e41fc4cef5b92f
haitaodesign <15393243613@163.com>
2017-01-13 20:41:27 +0800

    添加接口类ISystemUser

---
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ISystemManagement/ISystemUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.Model;
+
+namespace LHT.CRM.DAL.IServiceRespository.ISystemManagement
+{
+    public interface ISystemUser
+    {
+        List<SystemUser> GetAll();
+
+        void Add(SystemUser user);
+
+        void Delete(int userId);
+
+        void Update(SystemUser user);
+
+    }
+}
