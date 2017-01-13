d04e58c1406459055805059f42c0df536d9cc3ba
haitaodesign <15393243613@163.com>
2017-01-13 20:48:13 +0800

    添加ISystemRole接口类

---
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ISystemManagement/ISystemRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.Model;
+
+namespace LHT.CRM.DAL.IServiceRespository.ISystemManagement
+{
+    public interface ISystemRole
+    {
+        List<SystemRole> GetAll();
+
+        void Add(SystemRole role);
+
+        void Delete(int roleId);
+
+        void Update(SystemRole role);
+    }
+}
