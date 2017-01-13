e49bbf25e5f24ecfe5246823cdb1c305007c0f7c
haitaodesign <15393243613@163.com>
2017-01-13 19:47:46 +0800

    实现MD5字符串加密，加密验证

---
+++ LHT.CRM/LHT.CRM.Common/Encrypt/MD5Encrypt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace LHT.CRM.Common.Encrypt
+{
+    public class MD5Encrypt
+    {
+        //对一段字符串进行MD5加密
+
+        public static string GetMD5Hash(string password)
+        {
+            using(MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        //验证加密字符串方法
+
+        public static bool VeriyMd5Hash(string password,string hash)
+        {
+            string hashOfPassword = GetMD5Hash(password);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (comparer.Compare(hashOfPassword, hash)==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
