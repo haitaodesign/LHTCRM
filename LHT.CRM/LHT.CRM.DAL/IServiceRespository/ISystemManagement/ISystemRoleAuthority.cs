07b6b85bbf61a8358635899558c8bb636d528bd4
haitaodesign <15393243613@163.com>
2017-01-13 20:51:55 +0800

    增加ISystemRoleAuthority接口类CRUD

--- LHT.CRM/LHT.CRM.DAL/IServiceRespository/ISystemManagement/ISystemRoleAuthority.cs
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ISystemManagement/ISystemRoleAuthority.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHT.CRM.Model;
 
 namespace LHT.CRM.DAL.IServiceRespository.ISystemManagement
 {
     public interface ISystemRoleAuthority
     {
+        List<SystemRoleAuthority> GetAll();
 
+        List<SystemRoleAuthority> GetAll(int roleId);
+
+        void Add(SystemRoleAuthority roleAuth);
+
+        void Delete(int roleAuthId);
+
+        void Update(SystemRoleAuthority roleAuth);
     }
 }
