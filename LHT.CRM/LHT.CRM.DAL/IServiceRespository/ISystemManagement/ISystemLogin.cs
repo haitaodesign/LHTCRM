c61532cfd0a33c59e0bb36c2b7d981834c116f01
haitaodesign <15393243613@163.com>
2017-01-13 16:07:11 +0800

    实现ISystemLogin接口

---
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ISystemManagement/ISystemLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHT.CRM.DAL.IServiceRespository.ISystemManagement
+{
+    public interface ISystemLogin
+    {
+        List<SystemLogin> GetAll();
+
+        void Add(SystemLogin login);
+
+        void Delete(int loginId);
+
+        void Update(SystemLogin login);
+    }
+}
---
+++ LHT.CRM/LHT.CRM.DAL/ServiceRespository/SystemManagement/SystemLogin.cs
@@ -0,0 +1,42 @@
+﻿using LHT.CRM.DAL.IServiceRespository.ISystemManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHT.CRM.DAL.ServiceRespository.SystemManagement
+{
+    public class SystemLogin : LHTCRMBaseRespository<LHTCRMEntities>, ISystemLogin
+    {
+        public SystemLogin(LHTCRMEntities context) : base(context)
+        {
+        }
+
+        public SystemLogin():base(new LHTCRMEntities())
+        {
+        }
+
+
+
+        public void Add(DAL.SystemLogin login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int loginId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DAL.SystemLogin> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DAL.SystemLogin login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
