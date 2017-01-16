3a4832c34dacb7d29dfe723ec995ce1d175b43dc
haitaodesign <15393243613@163.com>
2017-01-16 20:45:33 +0800

    LHTCRM主界面设计

--- LHT.CRM/LHT.CRM.App/frmMain.Designer.cs
+++ LHT.CRM/LHT.CRM.App/frmMain.Designer.cs
@@ -28,23 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.skinListBox1 = new CCWin.SkinControl.SkinListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(8, 39);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.skinListBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(142)))), ((int)(((byte)(205)))));
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1264, 913);
+            this.splitContainer1.SplitterDistance = 240;
+            this.splitContainer1.SplitterWidth = 2;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // skinListBox1
+            // 
+            this.skinListBox1.Back = null;
+            this.skinListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(142)))), ((int)(((byte)(205)))));
+            this.skinListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.skinListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.skinListBox1.Font = new System.Drawing.Font("微软雅黑 Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinListBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.skinListBox1.FormattingEnabled = true;
+            this.skinListBox1.ItemBorderVisble = false;
+            this.skinListBox1.ItemGlassVisble = false;
+            this.skinListBox1.ItemHeight = 40;
+            this.skinListBox1.ItemRadius = 4;
+            this.skinListBox1.Items.AddRange(new CCWin.SkinControl.SkinListBoxItem[] {
+            ((CCWin.SkinControl.SkinListBoxItem)(resources.GetObject("skinListBox1.Items"))),
+            ((CCWin.SkinControl.SkinListBoxItem)(resources.GetObject("skinListBox1.Items1")))});
+            this.skinListBox1.Location = new System.Drawing.Point(0, 0);
+            this.skinListBox1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.skinListBox1.MouseColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(160)))), ((int)(((byte)(230)))));
+            this.skinListBox1.Name = "skinListBox1";
+            this.skinListBox1.RowBackColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(142)))), ((int)(((byte)(205)))));
+            this.skinListBox1.RowBackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(142)))), ((int)(((byte)(205)))));
+            this.skinListBox1.SelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(125)))), ((int)(((byte)(192)))));
+            this.skinListBox1.Size = new System.Drawing.Size(240, 913);
+            this.skinListBox1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(142)))), ((int)(((byte)(205)))));
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.White;
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.Color.White;
+            this.splitContainer2.Size = new System.Drawing.Size(1022, 913);
+            this.splitContainer2.SplitterDistance = 56;
+            this.splitContainer2.SplitterWidth = 1;
+            this.splitContainer2.TabIndex = 0;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 35F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1067, 637);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(117)))), ((int)(((byte)(180)))));
+            this.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(117)))), ((int)(((byte)(180)))));
+            this.CaptionBackColorBottom = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(117)))), ((int)(((byte)(180)))));
+            this.CaptionBackColorTop = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(117)))), ((int)(((byte)(180)))));
+            this.ClientSize = new System.Drawing.Size(1280, 960);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("微软雅黑 Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "frmMain";
             this.Shadow = false;
-            this.Text = "Form1";
+            this.ShowBorder = false;
+            this.ShowDrawIcon = false;
+            this.Text = "LHTCRM";
+            this.TitleColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private CCWin.SkinControl.SkinListBox skinListBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
     }
 }
 
