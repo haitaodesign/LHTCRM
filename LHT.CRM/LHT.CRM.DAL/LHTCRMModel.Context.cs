39bf64d7bdfbec39b7f265023de95e483235f5bc
haitaodesign <15393243613@163.com>
2017-03-03 22:40:10 +0800

    新建销售目标表，并更新到EF实体模型

--- LHT.CRM/LHT.CRM.DAL/LHTCRMModel.Context.cs
+++ LHT.CRM/LHT.CRM.DAL/LHTCRMModel.Context.cs
@@ -34,5 +34,6 @@ namespace LHT.CRM.DAL
         public virtual DbSet<CRM_Customer> CRM_Customer { get; set; }
         public virtual DbSet<CRM_Contact> CRM_Contact { get; set; }
         public virtual DbSet<CRM_Visit> CRM_Visit { get; set; }
+        public virtual DbSet<CRM_Target> CRM_Target { get; set; }
     }
 }
