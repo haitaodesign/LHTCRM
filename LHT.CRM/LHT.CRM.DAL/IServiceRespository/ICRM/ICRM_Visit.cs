198194a7f864d6db11e9af813a5f4c6f8870bfcf
haitaodesign <15393243613@163.com>
2017-03-04 20:17:52 +0800

    完成CRM业务表访问接口

---
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Visit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHT.CRM.DAL.IServiceRespository.ICRM
+{
+    interface ICRM_Visit
+    {
+    }
+}
