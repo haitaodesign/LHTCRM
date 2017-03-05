45753c3ea59ce3a909c7de4111dfbac22085645c
haitaodesign <15393243613@163.com>
2017-03-05 14:52:52 +0800

    Revert "CRM业务表接口方法开发完成。增删改查"
    This reverts commit beaeb680e698ffaec6ee2d97475f465f9f109d68.

--- LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Target.cs
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Target.cs
@@ -1,5 +1,4 @@
-﻿using LHT.CRM.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +8,5 @@ namespace LHT.CRM.DAL.IServiceRespository.ICRM
 {
     interface ICRM_Target
     {
-        List<CRM_Contact> GetAll();
-
-        CRM_Contact GetModel(string Name);
-
-        void Add(CRM_Contact contact);
-
-        void Delete(int id);
-
-        void Update(CRM_Contact contact);
     }
 }
--- LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Visit.cs
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Visit.cs
@@ -1,5 +1,4 @@
-﻿using LHT.CRM.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +8,5 @@ namespace LHT.CRM.DAL.IServiceRespository.ICRM
 {
     interface ICRM_Visit
     {
-        List<CRM_Visit> GetAll();
-
-        CRM_Visit GetModel(int id);
-
-        void Add(CRM_Visit visit);
-
-        void Delete(int id);
-
-        void Update(CRM_Visit visit);
     }
 }
