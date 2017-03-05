988b50ea7945e459458bb8e5ebb80ad251e96cc5
haitaodesign <15393243613@163.com>
2017-03-05 21:05:06 +0800

    完成客户信息表数据访问类的CRUD

--- LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Customer.cs
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Customer.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHT.CRM.Model;
 
 namespace LHT.CRM.DAL.IServiceRespository.ICRM
 {
     interface ICRM_Customer
     {
+        List<CRM_Customer> GetAll();
+
+        void Add(CRM_Customer customer);
+
+        void Delete(int cusId);
+
+        void Update(CRM_Customer customer);
     }
 }
---
+++ LHT.CRM/LHT.CRM.DAL/ServiceRespository/CRM/CRM_CustomerService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.DAL.IServiceRespository.ICRM;
+using LHT.CRM.Model;
+
+namespace LHT.CRM.DAL.ServiceRespository.CRM
+{
+    public class CRM_CustomerService : LHTCRMBaseRespository<LHTCRMEntities>, ICRM_Customer
+    {
+        public CRM_CustomerService(LHTCRMEntities context) : base(context) { }
+        public CRM_CustomerService() : base(new LHTCRMEntities()) { }
+
+        public void Add(CRM_Customer customer)
+        {
+            if (customer != null)
+            {
+                _dbContext.CRM_Customer.Add(customer);
+            }
+        }
+
+        public void Delete(int cusId)
+        {
+            var customer = _dbContext.CRM_Customer.Find(cusId);
+            if (customer != null)
+            {
+                _dbContext.CRM_Customer.Remove(customer);
+            }
+        }
+
+        public List<CRM_Customer> GetAll()
+        {
+            return _dbContext.CRM_Customer.ToList();
+        }
+
+        public void Update(CRM_Customer customer)
+        {
+            var updatecustomer = _dbContext.CRM_Customer.Find(customer.CusCode);
+            if (updatecustomer != null)
+            {
+                updatecustomer.MId = customer.MId;
+                updatecustomer.MDate = customer.MDate;
+                updatecustomer.Province = customer.Province;
+                updatecustomer.City = customer.City;
+                updatecustomer.CusName = customer.CusName;
+                updatecustomer.BussinessType = customer.BussinessType;
+                updatecustomer.CusType = customer.CusType;
+                updatecustomer.HomePage = customer.HomePage;
+                updatecustomer.PhoneNum = customer.PhoneNum;
+                updatecustomer.SaleId = customer.SaleId;
+                updatecustomer.IsPublisCus = customer.IsPublisCus;
+                updatecustomer.Scale = customer.Scale;
+            }
+        }
+    }
+}
