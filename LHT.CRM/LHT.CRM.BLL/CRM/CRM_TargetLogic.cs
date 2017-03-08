7caf60b4aa02e6a9441bd6a35eeff082a5e30600
haitaodesign <15393243613@163.com>
2017-03-08 20:37:18 +0800

    目标管理列表显示，实现显示列表信息方法

---
+++ LHT.CRM/LHT.CRM.BLL/CRM/CRM_TargetLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.DAL.ServiceRespository.CRM;
+using LHT.CRM.Model;
+
+namespace LHT.CRM.BLL.CRM
+{
+    public class CRM_TargetLogic
+    {
+        CRM_TargetService cts = new CRM_TargetService();
+        public List<CRM_Target> GetAll()
+        {
+            return cts.GetAll();
+        }   
+    }
+}
---
+++ LHT.CRM/LHT.CRM.DAL/ServiceRespository/CRM/CRM_TargetService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.DAL.IServiceRespository.ICRM;
+using LHT.CRM.Model;
+
+namespace LHT.CRM.DAL.ServiceRespository.CRM
+{
+    public class CRM_TargetService : LHTCRMBaseRespository<LHTCRMEntities>, ICRM_Target
+    {
+        public CRM_TargetService(LHTCRMEntities context) : base(context) { }
+
+        public CRM_TargetService():base(new LHTCRMEntities()) { }
+
+        public void Add(CRM_Target target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int targetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CRM_Target> GetAll()
+        {
+            return _dbContext.CRM_Target.ToList();
+        }
+
+        public void Update(CRM_Target target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
