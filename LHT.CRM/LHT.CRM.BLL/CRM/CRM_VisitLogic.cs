737669487d3b432e49cda3f120dcdb624715fdd5
haitaodesign <15393243613@163.com>
2017-03-09 23:24:16 +0800

    提交一次BLL层新建CRM_VisitLogic类

---
+++ LHT.CRM/LHT.CRM.BLL/CRM/CRM_VisitLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHT.CRM.BLL.CRM
+{
+    public class CRM_VisitLogic
+    {
+
+    }
+}
