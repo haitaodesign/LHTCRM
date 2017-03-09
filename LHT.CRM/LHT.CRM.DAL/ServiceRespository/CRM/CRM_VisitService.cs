33aa55121ee84be4aa03f3d0a6925d925dfe720f
haitaodesign <15393243613@163.com>
2017-03-09 23:13:19 +0800

    完成Linq查询多表单元测试

---
+++ LHT.CRM/LHT.CRM.DAL/ServiceRespository/CRM/CRM_VisitService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.DAL.IServiceRespository.ICRM;
+using LHT.CRM.Model;
+
+namespace LHT.CRM.DAL.ServiceRespository.CRM
+{
+    public class CRM_VisitService : LHTCRMBaseRespository<LHTCRMEntities>, ICRM_Visit
+    {
+        public CRM_VisitService(LHTCRMEntities context) : base(context)
+        {
+        }
+        public CRM_VisitService():base(new LHTCRMEntities()) { }
+
+        public List<CRM_Visit> GetAll()
+        {
+            return _dbContext.CRM_Visit.ToList();
+        }
+
+        public void Add(CRM_Visit visit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int visitId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CRM_Visit visit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LHT.CRM/LHT.CRM.DALUnitTestProject/UnitTestVisitDAL.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LHT.CRM.DAL.ServiceRespository.CRM;
+using LHT.CRM.DAL.ServiceRespository.SystemManagement;
+
+namespace LHT.CRM.DALUnitTestProject
+{
+    [TestClass]
+    public class UnitTestVisitDAL
+    {
+        CRM_VisitService cvs = new CRM_VisitService();
+        SystemUserService sus = new SystemUserService();
+        CRM_CustomerService ccs = new CRM_CustomerService();
+        /// <summary>
+        /// 返回拜访管理的字段
+        /// </summary>
+        [TestMethod]
+        public void GetVistList()
+        {
+
+            var query = from c in cvs.GetAll()
+                        join s in sus.GetAll()
+                        on c.VSuperiorId equals s.Id into cs
+                        from css in cs.DefaultIfEmpty()
+                        select new
+                        {
+                            Id = c.VSuperiorId,
+                            Name = css.UserName
+                        };
+            
+
+
+            Assert.IsNotNull(query);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+           
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+            var query2 = from c in cvs.GetAll()
+                         join customer in ccs.GetAll()
+                         on c.CusId equals customer.Id into cvcs
+                         from vcs in cvcs.DefaultIfEmpty()
+                         select new
+                         {
+                             CusId = c.CusId,
+                             CusCode = vcs.CusCode,
+                             CusName = vcs.CusName,
+                         };
+
+            Assert.IsNotNull(query2);
+
+            //构造一个显示列表的类
+
+
+            foreach (var item in query2)
+            {
+                Console.WriteLine(item.CusId+item.CusCode+item.CusName);
+            }
+          
+        }
+
+        [TestMethod]
+        public void ThreeTableJoin()
+        {
+            var query3 = from visit in cvs.GetAll()
+                         join customer in ccs.GetAll()
+                         on visit.CusId equals customer.Id
+                         join user in sus.GetAll()
+                         on visit.VSuperiorId equals user.Id
+                         select new
+                         {
+                             CusCode = customer.CusCode,
+                             CusName = customer.CusName,
+                             UserName = user.UserName
+                         };
+            Assert.IsNotNull(query3);
+            foreach (var item in query3)
+            {
+                Console.WriteLine(item.CusCode+"-"+item.CusName+"-"+item.UserName);
+            }
+        }
+    }
+}
