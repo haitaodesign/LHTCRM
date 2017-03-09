10b8ede0db4769ff95b6963dbc98273fadc3ce91
haitaodesign <15393243613@163.com>
2017-03-09 20:11:58 +0800

    拜访管理CRUD接口方法开发完成

--- LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Visit.cs
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Visit.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHT.CRM.Model;
 
 namespace LHT.CRM.DAL.IServiceRespository.ICRM
 {
     interface ICRM_Visit
     {
+        List<CRM_Visit> GetAll();
+
+        void Add(CRM_Visit visit);
+
+        void Delete(int visitId);
+
+        void Update(CRM_Visit visit);
     }
 }
