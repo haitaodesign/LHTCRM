fa56e0dc2b44cb4aa481eef41e5c0d8e0969dbbf
haitaodesign <15393243613@163.com>
2017-03-10 21:16:59 +0800

    产品管理接口方法CRUD开发完成

--- LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Product.cs
+++ LHT.CRM/LHT.CRM.DAL/IServiceRespository/ICRM/ICRM_Product.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LHT.CRM.Model;
 
 namespace LHT.CRM.DAL.IServiceRespository.ICRM
 {
     interface ICRM_Product
     {
+        List<CRM_Product> GetAll();
+
+        void Add(CRM_Product product);
+
+        void Delete(int productId);
+
+        void Update(CRM_Product product);
     }
 }
