4e646a5ac26f1fa6c1f0af07df56d33178b2c259
haitaodesign <15393243613@163.com>
2017-03-19 15:53:08 +0800

    添加产品信息表单开发完成

---
+++ LHT.CRM/LHT.CRM.App/CRM/frmCreateProduct.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace LHT.CRM.App.CRM
+{
+    partial class frmCreateProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new DMSkin.Controls.DMButton();
+            this.btnEnter = new DMSkin.Controls.DMButton();
+            this.tbIntroduction = new System.Windows.Forms.TextBox();
+            this.tbQuantity = new System.Windows.Forms.TextBox();
+            this.tbUnit = new System.Windows.Forms.TextBox();
+            this.tbCateGory = new System.Windows.Forms.TextBox();
+            this.tbProductName = new System.Windows.Forms.TextBox();
+            this.metroLabel6 = new DMSkin.Metro.Controls.MetroLabel();
+            this.metroLabel5 = new DMSkin.Metro.Controls.MetroLabel();
+            this.metroLabel4 = new DMSkin.Metro.Controls.MetroLabel();
+            this.metroLabel3 = new DMSkin.Metro.Controls.MetroLabel();
+            this.metroLabel1 = new DMSkin.Metro.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.DM_DisabledColor = System.Drawing.Color.Empty;
+            this.btnCancel.DM_DownColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(140)))), ((int)(((byte)(188)))));
+            this.btnCancel.DM_MoveColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(195)))), ((int)(((byte)(245)))));
+            this.btnCancel.DM_NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(163)))), ((int)(((byte)(220)))));
+            this.btnCancel.DM_Radius = 5;
+            this.btnCancel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Image = null;
+            this.btnCancel.Location = new System.Drawing.Point(405, 431);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(125, 45);
+            this.btnCancel.TabIndex = 51;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.BackColor = System.Drawing.Color.Transparent;
+            this.btnEnter.DM_DisabledColor = System.Drawing.Color.Empty;
+            this.btnEnter.DM_DownColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(140)))), ((int)(((byte)(188)))));
+            this.btnEnter.DM_MoveColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(195)))), ((int)(((byte)(245)))));
+            this.btnEnter.DM_NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(163)))), ((int)(((byte)(220)))));
+            this.btnEnter.DM_Radius = 5;
+            this.btnEnter.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnEnter.ForeColor = System.Drawing.Color.White;
+            this.btnEnter.Image = null;
+            this.btnEnter.Location = new System.Drawing.Point(174, 431);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(125, 45);
+            this.btnEnter.TabIndex = 50;
+            this.btnEnter.Text = "确定";
+            this.btnEnter.UseVisualStyleBackColor = false;
+            // 
+            // tbIntroduction
+            // 
+            this.tbIntroduction.Location = new System.Drawing.Point(244, 333);
+            this.tbIntroduction.Multiline = true;
+            this.tbIntroduction.Name = "tbIntroduction";
+            this.tbIntroduction.Size = new System.Drawing.Size(286, 50);
+            this.tbIntroduction.TabIndex = 49;
+            // 
+            // tbQuantity
+            // 
+            this.tbQuantity.Location = new System.Drawing.Point(244, 271);
+            this.tbQuantity.Name = "tbQuantity";
+            this.tbQuantity.Size = new System.Drawing.Size(286, 25);
+            this.tbQuantity.TabIndex = 48;
+            // 
+            // tbUnit
+            // 
+            this.tbUnit.Location = new System.Drawing.Point(244, 209);
+            this.tbUnit.Name = "tbUnit";
+            this.tbUnit.Size = new System.Drawing.Size(286, 25);
+            this.tbUnit.TabIndex = 47;
+            // 
+            // tbCateGory
+            // 
+            this.tbCateGory.Location = new System.Drawing.Point(244, 147);
+            this.tbCateGory.Name = "tbCateGory";
+            this.tbCateGory.Size = new System.Drawing.Size(286, 25);
+            this.tbCateGory.TabIndex = 46;
+            // 
+            // tbProductName
+            // 
+            this.tbProductName.Location = new System.Drawing.Point(244, 85);
+            this.tbProductName.Name = "tbProductName";
+            this.tbProductName.Size = new System.Drawing.Size(286, 25);
+            this.tbProductName.TabIndex = 45;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.DM_FontSize = DMSkin.Metro.MetroLabelSize.Tall;
+            this.metroLabel6.Location = new System.Drawing.Point(99, 333);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(50, 25);
+            this.metroLabel6.TabIndex = 44;
+            this.metroLabel6.Text = "描述";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.DM_FontSize = DMSkin.Metro.MetroLabelSize.Tall;
+            this.metroLabel5.Location = new System.Drawing.Point(99, 271);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(50, 25);
+            this.metroLabel5.TabIndex = 43;
+            this.metroLabel5.Text = "数量";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.DM_FontSize = DMSkin.Metro.MetroLabelSize.Tall;
+            this.metroLabel4.Location = new System.Drawing.Point(99, 209);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(50, 25);
+            this.metroLabel4.TabIndex = 42;
+            this.metroLabel4.Text = "单位";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.DM_FontSize = DMSkin.Metro.MetroLabelSize.Tall;
+            this.metroLabel3.Location = new System.Drawing.Point(99, 147);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(50, 25);
+            this.metroLabel3.TabIndex = 41;
+            this.metroLabel3.Text = "类别";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.DM_FontSize = DMSkin.Metro.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(99, 85);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(88, 25);
+            this.metroLabel1.TabIndex = 40;
+            this.metroLabel1.Text = "产品名称";
+            // 
+            // frmCreateProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderColor = System.Drawing.Color.White;
+            this.CaptionBackColorBottom = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(117)))), ((int)(((byte)(180)))));
+            this.CaptionBackColorTop = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(117)))), ((int)(((byte)(180)))));
+            this.ClientSize = new System.Drawing.Size(643, 501);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.tbIntroduction);
+            this.Controls.Add(this.tbQuantity);
+            this.Controls.Add(this.tbUnit);
+            this.Controls.Add(this.tbCateGory);
+            this.Controls.Add(this.tbProductName);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCreateProduct";
+            this.ShowDrawIcon = false;
+            this.Text = "添加产品信息";
+            this.TitleColor = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DMSkin.Controls.DMButton btnCancel;
+        private DMSkin.Controls.DMButton btnEnter;
+        private System.Windows.Forms.TextBox tbIntroduction;
+        private System.Windows.Forms.TextBox tbQuantity;
+        private System.Windows.Forms.TextBox tbUnit;
+        private System.Windows.Forms.TextBox tbCateGory;
+        private System.Windows.Forms.TextBox tbProductName;
+        private DMSkin.Metro.Controls.MetroLabel metroLabel6;
+        private DMSkin.Metro.Controls.MetroLabel metroLabel5;
+        private DMSkin.Metro.Controls.MetroLabel metroLabel4;
+        private DMSkin.Metro.Controls.MetroLabel metroLabel3;
+        private DMSkin.Metro.Controls.MetroLabel metroLabel1;
+    }
+}
