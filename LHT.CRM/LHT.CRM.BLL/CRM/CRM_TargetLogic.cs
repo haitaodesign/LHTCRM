42609e52d235df6aa04d3c56cc45afe5d605346d
haitaodesign <15393243613@163.com>
2017-03-21 20:33:11 +0800

    销售目标BLL层CRUD开发完成

--- LHT.CRM/LHT.CRM.BLL/CRM/CRM_TargetLogic.cs
+++ LHT.CRM/LHT.CRM.BLL/CRM/CRM_TargetLogic.cs
@@ -11,10 +11,57 @@ namespace LHT.CRM.BLL.CRM
     public class CRM_TargetLogic
     {
         CRM_TargetService cts = new CRM_TargetService();
+
+
+
         public List<CRM_Target> GetAll()
         {
             return cts.GetAll();
         }
-           
+
+
+        public int Add(CRM_Target target)
+        {
+            cts.Add(target);
+            int result = cts.SaveChanges();
+            if (result > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        public int Delete(int targetId)
+        {
+            cts.Delete(targetId);
+            int result = cts.SaveChanges();
+            if (result > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Update(CRM_Target target)
+        {
+            cts.Update(target);
+            int result = cts.SaveChanges();
+            if (result > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
     }
 }
